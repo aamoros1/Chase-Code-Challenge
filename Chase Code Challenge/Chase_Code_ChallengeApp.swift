0e7f3f843f2eec5041eb315cc3557d73288b5879
Alex <aalex776@protonmail.com>
2023-05-25 06:13:21 -0400

    Initial Commit

---
+++ Chase Code Challenge/Chase_Code_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chase_Code_ChallengeApp.swift
+//  Chase Code Challenge
+//
+//  Created by Chris on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Chase_Code_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
